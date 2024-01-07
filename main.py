3f202348de10a7f34de5c9519d8ac01254934605
Luis Moraes <lftmoraes@gmail.com>
2024-01-07 01:41:15 -0600

    rough draft

---
+++ main.py
@@ -0,0 +1,22 @@
+import csv
+import requests
+from argparse import ArgumentParser
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+def run(**kwargs):
+
+  # make requests and accumulate output
+
+  # parse outputs
+
+  # write csv
+
+  pass
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser()
+  args = parser.parse_args()
+  run(**args) # to_dict()??
+
