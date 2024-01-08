c69d7f612b5df96f11f3310df5b6b41e32a7f611
Luis Moraes <lftmoraes@gmail.com>
2024-01-07 21:55:50 -0600

    idea is taking shape

--- main.py
+++ main.py
@@ -1,47 +1,95 @@
+import logging
 import csv
 import json
 import requests
 from argparse import ArgumentParser
 from tqdm import tqdm
 from bs4 import BeautifulSoup
+from time import time
 
-def extract_data(request):
-''' extracts all relevant data from raw request '''
-# response is in JSON format
-# "results_html" attribute is an HTML string
 
-def scrape_reviews(curator:str):
+def extract_data(html_fragment:str):
+''' extracts all relevant data from html fragment '''
+# use beautifulsoup to parse? use regex?
+
+
+def has_error(response:str):
+''' returns true if there is an issue with the response '''
+
+  if response.status_code != 200: # requires raw response... maybe convert to "is None" check (and return None when status code != 200)?
+    return True
+  if response['success'] != '1':  # requires json parsing...
+    return True
+
+  return False
+
+
+def get_recommendations(curator:str, start:int, count:int):  # do we need option to inject session?
+''' wraps steam api for requesting a curator's recommendations '''
+
+  # just need some nice way of writing this...
+  base_url = f"https://store.steampowered.com/curator/{curator}/ajaxgetfilteredrecommendations/"
+  params = f"?query&start={start}&count={count}&dynamic_data=&tagids=&sort=recent&app_types=&curations=&reset=false"
+  query = base_url + params
+  try:
+
+    response = requests.get(query)
+    logging.debug("response received:\n%s", response)
+
+    json_object = json.loads(response)
+    logging.debug("json parsing successful")
+
+  except Exception as ex:
+    pass
+
+  return json_object
+
+
+def scrape_recommendations(curator:str):
 ''' makes all requests to scrape curator reviews '''
+
   start = 0
   count = 10
+
   while True:
 
-    query = f"https://store.steampowered.com/curator/{curator}/ajaxgetfilteredrecommendations/?query&start={start}&count={count}&dynamic_data=&tagids=&sort=recent&app_types=&curations=&reset=false"
-    # make request
-    response = requests.get(query)  # abstract to handle exceptions
-    # could be made into a yield
+    json_object = get_recommendations(curator, start, count)
+
+    # check for issues with response
+    if has_error(json_object):
+      # retry? handle error? raise error?
+      raise RuntimeError("Ran into issue with response")  # add stacktrace
+
+    # return response as an element of a generator
+    yield json_object
 
     # check if done
-    if start+count > response['total_count']:
+    if (start + count) >= json_object['total_count']:  # requires json parsing...
       break
 
     # prepare for next request
     start += count
 
     # insert random delay of approx 0.5s
+    time.sleep(0.5)
 
-  return # reviews?
+  return # end generator
+
 
 def run(**kwargs):
 
   #try:
-  # for each review_batch in scrape_reviews():
+  # for response_object in scrape_recommendations(curator):
+  #   recommendation_batch = extract_data(response_object.results_html)
   #   write lines to csv
   #except:
 
   pass
 
+
 if __name__ == "__main__":
+
+  # need an argument for 'curator'
 
   parser = argparse.ArgumentParser()
   args = parser.parse_args()
